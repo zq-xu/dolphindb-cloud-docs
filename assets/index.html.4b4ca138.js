5d6000d93fedf632d1ba0f80344325d6437f7292
zq-xu <46521145+zq-xu@users.noreply.github.com>
2022-10-11 08:57:51 +0000

    Deploying to gh-pages from @ zq-xu/dolphindb-cloud-docs@ce5fbdcc4cc3724eb9c877f48c2e96099643193a 🚀

---
+++ assets/index.html.4b4ca138.js
@@ -0,0 +1,23 @@
+import{_ as o,r as c,o as r,c as t,a as n,w as l,b as s,d as e}from"./app.d8efe043.js";const d={},p=s("h3",{id:"\u50CF\u6570-1-2-3-\u4E00\u6837\u5BB9\u6613",tabindex:"-1"},[s("a",{class:"header-anchor",href:"#\u50CF\u6570-1-2-3-\u4E00\u6837\u5BB9\u6613","aria-hidden":"true"},"#"),e(" \u50CF\u6570 1, 2, 3 \u4E00\u6837\u5BB9\u6613")],-1),u=s("div",{class:"language-bash ext-sh line-numbers-mode"},[s("pre",{class:"shiki",style:{"background-color":"#1E1E1E"}},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u5728\u4F60\u7684\u9879\u76EE\u4E2D\u5B89\u88C5")]),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#D4D4D4"}},"yarn add -D vuepress@next")]),e(`
+`),s("span",{class:"line"}),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u65B0\u5EFA\u4E00\u4E2A markdown \u6587\u4EF6")]),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#DCDCAA"}},"echo"),s("span",{style:{color:"#D4D4D4"}}," "),s("span",{style:{color:"#CE9178"}},"'# Hello VuePress'"),s("span",{style:{color:"#D4D4D4"}}," > README.md")]),e(`
+`),s("span",{class:"line"}),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u5F00\u59CB\u5199\u4F5C")]),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#D4D4D4"}},"yarn vuepress dev")]),e(`
+`),s("span",{class:"line"}),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u6784\u5EFA\u9759\u6001\u6587\u4EF6")]),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#D4D4D4"}},"yarn vuepress build")]),e(`
+`),s("span",{class:"line"})])]),s("div",{class:"line-numbers","aria-hidden":"true"},[s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"})])],-1),m=s("div",{class:"language-bash ext-sh line-numbers-mode"},[s("pre",{class:"shiki",style:{"background-color":"#1E1E1E"}},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u5728\u4F60\u7684\u9879\u76EE\u4E2D\u5B89\u88C5")]),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#D4D4D4"}},"npm install -D vuepress@next")]),e(`
+`),s("span",{class:"line"}),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u65B0\u5EFA\u4E00\u4E2A markdown \u6587\u4EF6")]),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#DCDCAA"}},"echo"),s("span",{style:{color:"#D4D4D4"}}," "),s("span",{style:{color:"#CE9178"}},"'# Hello VuePress'"),s("span",{style:{color:"#D4D4D4"}}," > README.md")]),e(`
+`),s("span",{class:"line"}),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u5F00\u59CB\u5199\u4F5C")]),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#D4D4D4"}},"npx vuepress dev")]),e(`
+`),s("span",{class:"line"}),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u6784\u5EFA\u9759\u6001\u6587\u4EF6")]),e(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#D4D4D4"}},"npx vuepress build")]),e(`
+`),s("span",{class:"line"})])]),s("div",{class:"line-numbers","aria-hidden":"true"},[s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"})])],-1);function v(D,b){const a=c("CodeGroupItem"),i=c("CodeGroup");return r(),t("div",null,[p,n(i,null,{default:l(()=>[n(a,{title:"YARN",active:""},{default:l(()=>[u]),_:1}),n(a,{title:"NPM"},{default:l(()=>[m]),_:1})]),_:1})])}const _=o(d,[["render",v],["__file","index.html.vue"]]);export{_ as default};
