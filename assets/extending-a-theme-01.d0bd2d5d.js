f9526f681d87cf2a351c5064e7a941280959fa8f
zq-xu <46521145+zq-xu@users.noreply.github.com>
2022-10-12 10:24:18 +0000

    Deploying to gh-pages from @ zq-xu/dolphindb-cloud-docs@70d92258b6152b0ea19ffde1e55ef082df480920 🚀

--- assets/extending-a-theme-01.d0bd2d5d.js
+++ assets/extending-a-theme-01.d0bd2d5d.js
@@ -1,1 +1,1 @@
-const o="/dolphindb-cloud-docs/docs/images/cookbook/extending-a-theme-01.png";export{o as _};
+const o="/dolphindb-cloud-docs/images/cookbook/extending-a-theme-01.png";export{o as _};
