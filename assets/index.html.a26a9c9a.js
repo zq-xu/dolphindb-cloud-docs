5b6322539c699fbe52c14f9ee6c2862fc78c1796
zq-xu <46521145+zq-xu@users.noreply.github.com>
2022-10-19 11:05:48 +0000

    Deploying to gh-pages from @ zq-xu/dolphindb-cloud-docs@426847081ef1afc30b8b87b72f75731a8daf3341 🚀

---
+++ assets/index.html.a26a9c9a.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-2dbdee11","path":"/zh/tutorial/dolphindb-mgr/","title":"\u4ECB\u7ECD","lang":"zh-CN","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1666177507000,"contributors":[{"name":"\u5F90\u5FD7\u5F3A","email":"zhiqiang.xu@dolphindb.com","commits":1}]},"filePathRelative":"zh/tutorial/dolphindb-mgr/README.md"}');export{t as data};
